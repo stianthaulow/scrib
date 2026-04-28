0b530f87b06c9271c3ac65fb14847f3fe0d38396
Stian Thaulow <stian@thaulow.me>
2026-04-28 17:19:01 +0200

    chore: bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
     minSdk = 24
     targetSdk = 36
     versionCode = 1
-    versionName = "0.1.3"
+    versionName = "0.1.4"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
