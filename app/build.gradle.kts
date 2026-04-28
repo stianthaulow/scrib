24b3a071ce7ab6e11a052576ba330979edd6ed82
Stian Thaulow <43840258+stianthaulow@users.noreply.github.com>
2026-04-28 17:01:49 +0200

    feat: remove foojay-resolver plugin (#7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
     minSdk = 24
     targetSdk = 36
     versionCode = 1
-    versionName = "0.1.2"
+    versionName = "0.1.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
