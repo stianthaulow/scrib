b78704c95e42bce1bc19f969703d24c140c12db0
Stian Thaulow <stian@thaulow.me>
2026-08-01 19:32:56 +0200

    chore: bump to 0.1.9 (versionCode 6)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
     applicationId = "dev.thaulow.scrib"
     minSdk = 24
     targetSdk = 36
-    versionCode = 5
-    versionName = "0.1.8"
+    versionCode = 6
+    versionName = "0.1.9"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
